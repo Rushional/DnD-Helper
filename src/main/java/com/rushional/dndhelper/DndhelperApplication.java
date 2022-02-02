fbf7a5ce089b09773993242cca932f560bd9981b
Rushional <Rushional2@gmail.com>
2022-02-02 15:40:06 +0300

    first commit

---
+++ src/main/java/com/rushional/dndhelper/DndhelperApplication.java
@@ -0,0 +1,13 @@
+package com.rushional.dndhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DndhelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DndhelperApplication.class, args);
+    }
+
+}
